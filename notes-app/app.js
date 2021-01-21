1b1f45e0baf487c5a86353dde9f75660ad86dba9
Aya Nassar <joodnassar70@gmail.com>
2021-01-21 14:15:51 +0200

    project

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,12 @@
 const validator = require ('validator')
 const yargs = require ('yargs')
 const fs = require('fs');
-fs.writeFileSync('notes.txt',' hi ')
-
-console.log(validator.isEmail('joodnassar70@gmail.com'))
 const chalk = require('chalk')
-console.log(chalk.green.bold.bgBlue('aya nassar'))
+const notes = require('./u.js')
+
+fs.writeFileSync('notes.txt',' hi ')
+console.log(validator.isEmail('joodnassar70@gmail.com'))
+//console.log(chalk.green.bold.bgBlue('aya nassar'))
 
 const cm = process.argv[2]
 console.log (cm)
@@ -15,3 +16,42 @@ if (cm === 'add'){
   }  else if ( cm === 'remove '){
       console.log('removing node')
   }
+yargs.version('16.2.0')
+  yargs.command ({
+      command:'remove',
+      describe: 'remove the note ',
+     builder :{
+             title :{
+                 dicsribe: 'not title ',
+                 demandoption : true ,
+                 type : string 
+             }
+     },
+
+      handler: function (){
+          console.log('removing the note ')
+      }
+  })
+
+  yargs.command ({
+    command:'add',
+    describe: 'add the note ',
+    builder :{ 
+        title: {
+            dicsribe : 'note title ',
+            demandoption : true
+        }
+    },
+    body :{
+        dicsribe : 'note body',
+        demandOption: true ,
+        type :'string'
+
+    },
+    handler : function (argv){
+        console.log('adding the note ' ,argv) 
+        notes.addnote(argv.title,argv.body)
+    }
+})
+
+console.log(yargs.argv)
